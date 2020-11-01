26208c0db9392a85a35dfb8675b509607fa9a0be
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:40:18 +0530

    Create main.go

---
+++ Beginners/workshop/20_struct/05_promotion/01_overriding-fields/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+type doubleZero struct {
+	person
+	First         string
+	LicenseToKill bool
+}
+
+func main() {
+	p1 := doubleZero{
+		person: person{
+			First: "James",
+			Last:  "Bond",
+			Age:   20,
+		},
+		First:         "Double Zero Seven",
+		LicenseToKill: true,
+	}
+
+	p2 := doubleZero{
+		person: person{
+			First: "Miss",
+			Last:  "MoneyPenny",
+			Age:   19,
+		},
+		First:         "If looks could kill",
+		LicenseToKill: false,
+	}
+
+	// fields and methods of the inner-type are promoted to the outer-type
+	fmt.Println(p1.First, p1.person.First)
+	fmt.Println(p2.First, p2.person.First)
+}
