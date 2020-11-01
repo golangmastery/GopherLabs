d60a6a4d91e365c9127e145cd8acd1ddcd2159f6
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:37:27 +0530

    Create main.go

---
+++ Beginners/workshop/18_slice/08_append/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	greeting := make([]string, 3, 5)
+	// 3 is length - number of elements referred to by the slice
+	// 5 is capacity - number of elements in the underlying array
+
+	greeting[0] = "Good morning!"
+	greeting[1] = "Bonjour!"
+	greeting[2] = "buenos dias!"
+	greeting = append(greeting, "Suprabadham")
+
+	fmt.Println(greeting[3])
+}
