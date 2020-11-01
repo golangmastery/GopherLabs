5bd6d9ceaa86befdccd214d3743251a269b26724
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:38:37 +0530

    Create main.go

---
+++ Beginners/workshop/19_map/10_comma-ok-idiom_val-exists/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	myGreeting := map[int]string{
+		0: "Good morning!",
+		1: "Bonjour!",
+		2: "Buenos dias!",
+		3: "Bongiorno!",
+	}
+
+	fmt.Println(myGreeting)
+
+	// delete(myGreeting, 2)
+
+	if val, exists := myGreeting[2]; exists {
+		fmt.Println("That value exists.")
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+	} else {
+		fmt.Println("That value doesn't exist.")
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+	}
+
+	fmt.Println(myGreeting)
+}
