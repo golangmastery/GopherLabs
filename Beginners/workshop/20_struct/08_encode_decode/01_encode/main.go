cd5ca3827262bd1f7be9ba9430b3ea3598c199c3
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:26:06 +0530

    Create main.go

---
+++ Beginners/workshop/20_struct/08_encode_decode/01_encode/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type person struct {
+	First       string
+	Last        string
+	Age         int
+	notExported int
+}
+
+func main() {
+	p1 := person{"James", "Bond", 20, 007}
+	json.NewEncoder(os.Stdout).Encode(p1)
+}
