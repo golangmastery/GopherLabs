b5d4721a21e0f73a879ed0b1df11e20b6aa5195e
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:39:34 +0530

    Create main.go

---
+++ Beginners/workshop/19_map/14_hash-table/04_english-alphabet/01/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bs, _ := ioutil.ReadAll(res.Body)
+	str := string(bs)
+	fmt.Println(str)
+
+}
