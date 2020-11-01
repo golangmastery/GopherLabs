19d67553a6cbcce47603a83ab84a7b119a96ea50
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:23:38 +0530

    Create main.go

---
+++ Beginners/workshop/22_go-routines/09_channels/05_channel-direction/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := incrementor()
+	cSum := puller(c)
+	for n := range cSum {
+		fmt.Println(n)
+	}
+}
+
+func incrementor() <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			out <- i
+		}
+		close(out)
+	}()
+	return out
+}
+
+func puller(c <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		var sum int
+		for n := range c {
+			sum += n
+		}
+		out <- sum
+		close(out)
+	}()
+	return out
+}
+
+/*
+The optional <- operator specifies the channel direction, send or receive.
+If no direction is given, the channel is bidirectional.
+https://golang.org/ref/spec#Channel_types
+*/
