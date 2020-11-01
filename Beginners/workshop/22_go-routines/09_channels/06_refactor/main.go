dcc05c5a32310616f3139410f1325a6dacc6a1fe
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:23:36 +0530

    Create main.go

---
+++ Beginners/workshop/22_go-routines/09_channels/06_refactor/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := incrementor()
+	for n := range puller(c) {
+		fmt.Println(n)
+	}
+}
+
+func incrementor() <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			out <- i
+		}
+		close(out)
+	}()
+	return out
+}
+
+func puller(c <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		var sum int
+		for n := range c {
+			sum += n
+		}
+		out <- sum
+		close(out)
+	}()
+	return out
+}
