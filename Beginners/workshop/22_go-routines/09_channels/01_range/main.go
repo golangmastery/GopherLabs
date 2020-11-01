8af3ff251c68988419b79faf6fa9ab5f14600e1d
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:23:58 +0530

    Create main.go

---
+++ Beginners/workshop/22_go-routines/09_channels/01_range/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for n := range c {
+		fmt.Println(n)
+	}
+}
