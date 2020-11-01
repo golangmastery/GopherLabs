3d39e6cc8df78948fb4dfdaae9c644d48a671669
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:32:55 +0530

    Create main.go

---
+++ Beginners/workshop/06_constants/01_constant/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const p = "death & taxes"
+
+func main() {
+
+	const q = 42
+
+	fmt.Println("p - ", p)
+	fmt.Println("q - ", q)
+}
+
+// a CONSTANT is a simple unchanging value
