3109423e69863481a2978950d089157dd52ddd75
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 20:42:21 +0530

    Update main.go

--- Beginners/workshop/05_blank-identifier/02_http-get_example/02_no-error-checking/main.go
+++ Beginners/workshop/05_blank-identifier/02_http-get_example/02_no-error-checking/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	res, _ := http.Get("http://www.geekwiseacademy.com/")
+	res, _ := http.Get("http://gopherlabs.kubedaily.com/")
 	page, _ := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	fmt.Printf("%s", page)
