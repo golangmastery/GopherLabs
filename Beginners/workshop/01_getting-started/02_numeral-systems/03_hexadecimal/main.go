3ff5288166a6a7718507306d2a54f31f19f1986f
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:31:05 +0530

    Create main.go

---
+++ Beginners/workshop/01_getting-started/02_numeral-systems/03_hexadecimal/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	//	fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	//	fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+}
