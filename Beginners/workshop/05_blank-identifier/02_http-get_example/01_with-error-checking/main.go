79a096456acb47b957a4a7182b7c14e9ad3e7d63
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:32:47 +0530

    Create main.go

---
+++ Beginners/workshop/05_blank-identifier/02_http-get_example/01_with-error-checking/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://www.geekwiseacademy.com/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	page, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", page)
+}
