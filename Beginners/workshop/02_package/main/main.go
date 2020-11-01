c590e0b998401eccbd636f900c19d07200d299fc
sangam14 <sbiradar@ced.alliance.edu.in>
2020-11-01 13:31:18 +0530

    Create main.go

---
+++ Beginners/workshop/02_package/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sangam14/GopherLabs/02_package/stringutil"
+	"github.com/sangam14/GopherLabs02_package/icomefromalaska"
+	
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
